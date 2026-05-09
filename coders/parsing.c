78d23cd421458b503f7a6516d68f1bc05c8766aa
parksomin72 <fatimazahraealawi@gmail.com>
2026-05-09 14:00:05 +0100

    .

--- coders/parsing.c
+++ coders/parsing.c
@@ -1,22 +1,17 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/time.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include "coders.h"
 
-typedef struct s_info {
-    int number_of_coders;
-    int time_to_burnout;
-    int time_to_compile;
-    int time_to_debug;
-    int time_to_refactor;
-    int number_of_compiles_required;
-    int dongle_cooldown;
-    char scheduler[10];
-} t_info;
+int fall_scheduler(char *s, t_info *info)
+{
+    if (strcmp(s, "fifo") == 0)
+        info->scheduler = 0;
+    else if (strcmp(s, "edf") == 0)
+        info->scheduler = 1;
+    else
+        return (0);
+    return (1);
+}
 
-int check(char **av)
+int check(char **av, t_info *info)
 {
     int i;
     int j;
@@ -33,22 +28,11 @@ int check(char **av)
         }
         i++;
     }
-
-    if (strcmp(av[i], "fifo") != 0 && strcmp(av[i], "edf") != 0)
+    if (!fall_scheduler(av[8], info))
         return (0);
     return (1);
 }
 
-char *ft_strcpy(char *dest, char *src) {
-    char *tmp;
-
-    tmp = dest;
-    while (*src != '\0') {
-        *tmp++ = *src++;
-    }
-    *tmp = '\0';
-    return (dest);
-}
 
 int ft_atoi(char *s, int *flag)
 {
@@ -92,18 +76,34 @@ int convert(char **av, t_info *coders_info)
     coders_info->time_to_refactor = info[4];
     coders_info->number_of_compiles_required = info[5];
     coders_info->dongle_cooldown = info[6];
-    // ft_strcpy(coders_info->scheduler, av[8]);
+    return (1);
+}
+
+int allocate(t_info *info, t_coder *coder, int size)
+{
+    coder = malloc(sizeof(t_coder) * size);
+    if (!coder)
+        return (0);
+    info.dongles = malloc(sizeof(t_dongles) * size);
+    if (!info.dongles) {
+        free(coder);
+        return (0);
+    }
     return (1);
 }
 
 int main(int ac, char **av)
 {
-    t_info coder_info;
+    int n;
+    t_info info;
+    t_coder *coder;
+    t_monitor monitor;
 
-    if (ac != 9 || !check(av) ||  !convert(av, &coder_info)) {
+    if (ac != 9 || !check(av, &info) ||  !convert(av, &info)) {
         write(2, "Error\n", 6);
         return (1);
     }
+    n = info.number_of_coders;
+    printf("number_of_coders = %d\n", n);
 
-    printf("ok!\n"); // just for test now
 }
